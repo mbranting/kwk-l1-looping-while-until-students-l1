01dbd4e2b33f9b63df58727f292e09625db7e386
mbranting <mckenna.branting@gmail.com>
2018-06-05 21:27:26 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
 def using_while
-	levitation_force = 6
-	
-	#your code here
-end
+  
+  force = 0
+  while force < 10
+    puts "Wingardium Leviosa"
+    force += 1
+  end
+end 
+using_while
 
 
---
+++ until.rb
@@ -0,0 +1,14 @@
+def using_until
+  
+  force = 0
+  
+    while force < 10 
+    puts "Wingardium Leviosa"
+    force += 1
+    puts "#{force} force level" 
+    
+  end
+  
+end 
+
+using_until
